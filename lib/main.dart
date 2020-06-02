1166984a6845c6515dec4df45ee9a1422f81c4b4
leopalmeiro <leonardo.palmeiro@babel.es>
2020-06-02 23:42:31 +0200

    add Followers and descriptions

--- lib/main.dart
+++ lib/main.dart
@@ -102,12 +102,12 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ),
                       CircleAvatar(
-                        backgroundColor: Colors.blue,
+                        backgroundColor: Colors.white70,
                         minRadius: 60.0,
                         child: CircleAvatar(
                           radius: 50.0,
-                          backgroundImage: NetworkImage(
-                              'https://randomuser.me/api/portraits/lego/0.jpg'),
+                          backgroundImage:
+                              AssetImage('assets/images/perfil-min.jpg'),
                         ),
                       ),
                       CircleAvatar(
@@ -132,7 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   Text(
-                    'Flutter Develop',
+                    'Flutter Developer',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 25,
@@ -141,8 +141,116 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
-            Text('text 01'),
-            Text('text 01'),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      color: Colors.deepOrange.shade300,
+                      child: ListTile(
+                        title: Text(
+                          '5000',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Followers',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                      child: ListTile(
+                        title: Text(
+                          '5000',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Following',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      'Email',
+                      style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'palmeiro.leonardo@gmail.com',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text(
+                      'GitHub',
+                      style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'https://github.com/leopalmeiro',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text(
+                      'Linkedin',
+                      style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'www.linkedin.com/in/leonardo-palmeiro-834a1755',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
